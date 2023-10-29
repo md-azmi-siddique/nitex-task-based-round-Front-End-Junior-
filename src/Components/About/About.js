ed0681e1b773bf312bb16d2c0300b6b384ea26ab
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-10-30 00:50:53 +0600

    about section peragraph added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import './About.css'
-import about from '../Assets/images/About/about.png'
+import React from "react";
+import "./About.css";
+import Card from "react-bootstrap/Card";
+import about from "../Assets/images/About/about.png";
 
 const About = () => {
-    return (
-        <div className='about'>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-md-5'>
-                        <h4>Vision</h4>
-                        <p>Our missinon and vision is to provide good quality and healthy food for all.</p>
-                    </div>
-                    <div className='col-md-5 img-section d-flex justify-content-center'>
-                        <img className='about-img' src={about} alt="" />
-                    </div>
-
-                </div>
-
-            </div>
-            
-        </div>
-    );
+  return (
+    <div className="about">
+      <div className="container">
+        <Card>
+          <img className="about-img" src={about} alt="" />
+          <Card.Body>
+            <Card.Text className="d-flex align-items-center">
+            Our singular and unwavering mission is to foster a meaningful connection between dedicated organic farmers and discerning consumers, all in pursuit of a shared aspiration: to provide consumers with access to wholesome, nourishing food. We are dedicated to bridging the gap between those who cultivate the earth with care and those who seek the benefits of truly healthy, sustainable, and organic produce. By facilitating this vital relationship, we aim to empower consumers to make informed choices about their food, promoting both well-being and the well-being of our planet.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default About;
