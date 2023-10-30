dd0575a90486f52fcc7a32f6a72ad3f9b1d59c88
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-10-30 20:59:29 +0600

    footer link and hover effect

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faInstagram, faEdgeLegacy, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faEdgeLegacy,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 import "./ContactUs.css";
 
 const ContactUs = () => {
-
   return (
     <div className="footer">
-        
       <div className="container my-5">
         <footer className="text-center text-lg-start text-white">
           <div className="container p-4 pb-0">
@@ -16,7 +19,9 @@ const ContactUs = () => {
               <div className="row">
                 <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
                   <h6 className="text-uppercase mb-4 font-weight-bold">
-                    Food House
+                    <Link className="footer-item" to="/">
+                      ORGANIC HOUSE
+                    </Link>
                   </h6>
                   <p>
                     Providing the better and organic food for you and your
@@ -28,7 +33,9 @@ const ContactUs = () => {
 
                 <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
                   <h6 className="text-uppercase mb-4 font-weight-bold">
-                    Products
+                    <Link className="footer-item" to="/products">
+                      PRODUCTS
+                    </Link>
                   </h6>
                   <p>Organic</p>
                   <p>Non Organic</p>
@@ -38,7 +45,9 @@ const ContactUs = () => {
 
                 <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
                   <h6 className="text-uppercase mb-4 font-weight-bold">
-                  <Link className="footer-item" to="/services">Services</Link>
+                    <Link className="footer-item" to="/services">
+                      SERVICES
+                    </Link>
                   </h6>
                   <p>Home Delivery</p>
                   <p>Waste Management</p>
@@ -55,7 +64,8 @@ const ContactUs = () => {
                     <i className="fas fa-home mr-3"></i> Dhaka, Bangladesh
                   </p>
                   <p>
-                    <i className="fas fa-envelope mr-3"></i> md.azmi.siddique@gmail.com
+                    <i className="fas fa-envelope mr-3"></i>{" "}
+                    md.azmi.siddique@gmail.com
                   </p>
                   <p>
                     <i className="fas fa-phone mr-3"></i> + 880 1882400479
@@ -69,23 +79,47 @@ const ContactUs = () => {
             <section className="p-3 pt-0">
               <div className="row justify-content-center">
                 <div className="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
-                  <FontAwesomeIcon className="icons" icon={faEdgeLegacy} size="2xl"/>
-                  <FontAwesomeIcon className="icons" icon={faFacebook} size="2xl" />
-                  <FontAwesomeIcon className="icons" icon={faInstagram} size="2xl" />
-                  <FontAwesomeIcon className="icons" icon={faTwitter} size="2xl" />
+                  <Link className="footer-item" to="/">
+                    <FontAwesomeIcon
+                      className="icons"
+                      icon={faEdgeLegacy}
+                      size="2xl"
+                    />
+                  </Link>
+                  <Link className="footer-item" to="/">
+                    <FontAwesomeIcon
+                      className="icons"
+                      icon={faFacebook}
+                      size="2xl"
+                    />
+                  </Link>
+                  <Link className="footer-item" to="/">
+                    <FontAwesomeIcon
+                      className="icons"
+                      icon={faInstagram}
+                      size="2xl"
+                    />
+                  </Link>
+                  <Link className="footer-item" to="/">
+                    <FontAwesomeIcon
+                      className="icons"
+                      icon={faTwitter}
+                      size="2xl"
+                    />
+                  </Link>
                 </div>
               </div>
               <div className="p-3 d-flex align-items-center justify-content-center">
-                    © 2023 Copyright:
-                    <a className="text-white" href="https://mdbootstrap.com/">
-                      Md Azmi Siddique
-                    </a>
-                  </div>
+                © 2023 Copyright:
+                <a className="text-white" href="https://mdbootstrap.com/">
+                  Md Azmi Siddique
+                </a>
+              </div>
             </section>
           </div>
         </footer>
       </div>
-      </div>
+    </div>
     // </div>
   );
 };
